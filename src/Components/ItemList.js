de31d4a8924015b7c47efdb9f836c464d3c695bc
Suvankit Sahoo <suvankit2001@gmail.com>
2022-12-18 14:01:05 +0530

    Changed Background image

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -5,15 +5,27 @@ import ExpenseFilter from "./NewItem/ExpenseFilter";
 import { useState } from "react";
 const ItemList = function (props) {
   const [selectedYear, setYear] = useState("");
+  
   const handleYearChange = (newYear) => {
     setYear(newYear);
   };
+  const noExpenseFoundStyle = {
+    color : "white",
+    fontSize: "large"
+  }
+  
+  const filteredItems = props.itemsArray.filter(item => {return item.date.getFullYear().toString() === selectedYear})
+
+  let itemsDisplayed = <h2 style={noExpenseFoundStyle}> No Expenses Found</h2> ;
+  if(filteredItems.length > 0){
+    itemsDisplayed = filteredItems.map(item => <Item key = {item.key} purchaseDate = {item.date} itemName = {item.name} itemPrice = {item.price}/>)
+  }
   return (
     <div>
       
       <Card className="expenses">
       <ExpenseFilter selected={selectedYear} onYearChange={handleYearChange} />
-      {props.itemsArray.map(item => <Item key = {item.key} purchaseDate = {item.date} itemName = {item.name} itemPrice = {item.price}/>)}
+      {itemsDisplayed}
       </Card>
     </div>
   );
